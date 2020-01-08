c396e5a33a3c955fcb3c73b80106511cc34eb671
longyj_pc <longyj_pc@longyj>
2020-01-08 17:18:10 +0800

    test

---
+++ src/main/java/com/longyj/aichufang/AichufangApplication.java
@@ -0,0 +1,13 @@
+package com.longyj.aichufang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichufangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AichufangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longyj/aichufang/AichufangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longyj.aichufang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichufangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
